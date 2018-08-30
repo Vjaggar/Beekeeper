5f517b9a2e468e6083b7bdb0a1e47d69202470f5
Vjaggar <97021428@qq.com>
2018-08-30 20:40:36 +0800

    0.19
    1.[ADD]可切换执行引擎功能
    2.[ADD]断点重跑时,之前的SET设置保留下来

--- conf/beekeeper_table.sql
+++ conf/beekeeper_table.sql
@@ -15,19 +15,19 @@ quit;
 
 drop table beekeeper_log;
 create table beekeeper_log(
-task_id bigint COMMENT '任务ID',
-exectime    int COMMENT '需执行的日期',
-table_name  VARCHAR(200) COMMENT '表名',
-hql_file VARCHAR(2000) COMMENT 'HQL文件路径',
-blockcnt int COMMENT '块编号',
-blockmess VARCHAR(2000) COMMENT '块备注',
-loop_cnt  int COMMENT '重跑次数编号',
-hql    blob  COMMENT '执行的HQL语句',
-start_time  double  COMMENT '开始时间(Linux时间戳)',
-over_time  double  COMMENT '结束时间(Linux时间戳)',
-use_time  double  COMMENT '花费时间(s)',
-task_status  int  COMMENT '任务状态(0:成功 -1:失败 1:正在进行)',
-error_mess blob COMMENT 'HQL报错信息'
+task_id     bigint        COMMENT '任务ID'                             ,
+exectime    int           COMMENT '需执行的日期'                       ,
+table_name  VARCHAR(200)  COMMENT '表名'                               ,
+hql_file    VARCHAR(2000) COMMENT 'HQL文件路径'                        ,
+blockcnt    int           COMMENT '块编号'                             ,
+blockmess   VARCHAR(2000) COMMENT '块备注'                             ,
+loop_cnt    int           COMMENT '重跑次数编号'                       ,
+hql         blob          COMMENT '执行的HQL语句'                      ,
+start_time  double        COMMENT '开始时间(Linux时间戳)'              ,
+over_time   double        COMMENT '结束时间(Linux时间戳)'              ,
+use_time    double        COMMENT '花费时间(s)'                        ,
+task_status int           COMMENT '任务状态(0:成功 -1:失败 1:正在进行)',
+error_mess  blob          COMMENT 'HQL报错信息'
 )
 default charset = utf8
 ;
